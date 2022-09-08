3d9e39f45ae3f5c89f1b2340d38aeb39de2d2c0a
Fathul Irfaan Abdillah <abdillahfathu@gmail.com>
2022-09-08 09:05:03 +0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Welcome to SvelteKit</h1>
+<p>
+	Visit <a class="link" href="https://kit.svelte.dev"
+		>kit.svelte.dev <span class="link__span">aaaaaa</span></a
+	> to read the documentation
+</p>
+
+<style lang="scss">
+</style>
